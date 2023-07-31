c55c9224ea8c3aa858d89571fa46472d439a8fbf
Masoodmsd <108340160+Masoodmsd@users.noreply.github.com>
2023-07-31 14:57:56 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x78c0cd4E2B381cc708cfBdeafE75cdbEC696C80A";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
